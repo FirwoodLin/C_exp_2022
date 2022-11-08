98d57553693fb0ce128157b2c9e002d99dafb9c8
FirwoodLin <53254449+FirwoodLin@users.noreply.github.com>
2022-11-08 12:44:11 +0800

    lab06 except ep

---
+++ Lab06/6-1.c
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Fir
+ * @Date: 2022-11-08 12:12:17
+ * @LastEditors: Fir
+ * @LastEditTime: 2022-11-08 12:44:11
+ */
+#include <stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",x);
+    char
+}
